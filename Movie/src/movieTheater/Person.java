f9ea90097e55b1f9cab54d0dc4f838469b408177
Kizzia Fabaan <fabaankizzia@gmail.com>
2023-09-24 14:59:52 +0800

    try

---
+++ Movie/src/movieTheater/Person.java
@@ -0,0 +1,33 @@
+package movieTheater;
+
+class Person {
+    private String name;
+    private int seatNumber;
+    private boolean ticketPurchased = false;
+
+    public Person(String name, int seatNumber) {
+        this.name = name;
+        this.seatNumber = seatNumber;
+    }
+
+    public void purchaseTicket() {
+        this.ticketPurchased = true;
+    }
+
+    public boolean hasTicket() {
+        return ticketPurchased;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nSeat Number: " + seatNumber;
+    }
+}
